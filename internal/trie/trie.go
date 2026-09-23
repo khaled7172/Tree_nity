d895588dde268e33beee175affafcb8bfc71bdf5
khhammou <khhammou@student.42.fr>
2026-09-23 20:31:39 +0300

    feat(trie): implement Add method for prefix registration

--- internal/trie/trie.go
+++ internal/trie/trie.go
@@ -24,14 +24,44 @@ var _ Matcher = (*PrefixTrie)(nil)
 func New() *PrefixTrie {
 	return &PrefixTrie{
 		Root: &TrieNode{
-			Children:    make(map[rune]*TrieNode),
-			Subscribers: []string{},
+			Children: make(map[rune]*TrieNode),
 		},
-		GlobalSubscribers: []string{},
 	}
 }
 
-func (t *PrefixTrie) Add(prefix string, clientID string) {}
+func (t *PrefixTrie) Add(prefix string, clientID string) {
+	t.mu.Lock()
+	defer t.mu.Unlock()
+
+	if prefix == "" {
+		for _, id := range t.GlobalSubscribers {
+			if id == clientID {
+				return
+			}
+		}
+		t.GlobalSubscribers = append(t.GlobalSubscribers, clientID)
+		return
+	}
+
+	curr := t.Root
+	for _, ch := range prefix {
+		next, exists := curr.Children[ch]
+		if !exists {
+			next = &TrieNode{
+				Children: make(map[rune]*TrieNode),
+			}
+			curr.Children[ch] = next
+		}
+		curr = next
+	}
+
+	for _, id := range curr.Subscribers {
+		if id == clientID {
+			return
+		}
+	}
+	curr.Subscribers = append(curr.Subscribers, clientID)
+}
 
 func (t *PrefixTrie) Remove(prefix string, clientID string) {}
 
